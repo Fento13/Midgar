0c7cdc7e408d021e1e81a1589b6533e44b7f45e8
javie <javiercaja83@gmail.com>
2025-09-25 16:01:35 +0200

    25-09-2025

---
+++ Backend/Java/CosasDeClase/Ejercicios/src/PiedraPapelTijera.java
@@ -0,0 +1,7 @@
+public class PiedraPapelTijera {
+    public static void main(String[] args) {
+   System.out.println("Hola Mundo");
+
+       
+    }
+}
---
+++ Backend/Java/POO/Interfaces/EjemploInterfaz/src/EjemploInterfaz.java
@@ -0,0 +1,6 @@
+public interface EjemploInterfaz {
+    public String Atributo1;
+    public int Numero2;
+
+
+}
