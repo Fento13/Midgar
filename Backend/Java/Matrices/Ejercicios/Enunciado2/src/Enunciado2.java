ac8f1633c3ecd3b33b0ded00f04ca6e3c9f46c54
Javier <fento.013@gmail.com>
2025-12-01 17:35:53 +0100

    01/12/2025

---
+++ Backend/Java/Matrices/Ejercicios/Enunciado2/src/Enunciado2.java
@@ -0,0 +1,40 @@
+import java.nio.channels.Pipe.SourceChannel;
+import java.util.Random;
+
+public class Enunciado2 {
+    public static void main(String[] args) {
+    boolean[][] matriz = new boolean[4][4];
+        Random random = new Random();
+        int mitad = matriz.length / 2; //en vez de 2, se pone matriz.length / 2 para que funcione en cualquier matriz cuadrada
+
+        System.out.println("Matriz original:");
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                matriz[i][j] = random.nextBoolean(); //esto lo he buscado, genera valores dependiendo de true o false
+            System.out.println(matriz[i][j] ? "Verdadero" : "Falso");
+            }
+            System.out.println(); // Salto de línea para separar filas
+        }
+
+    System.out.println("----------------------------------------------");        
+        for (int i = mitad; i < matriz.length;  i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+               if (matriz[i].length > mitad) {
+                //cambiar el valor de true a false y viceversa
+                matriz[i][j] = !matriz[i][j];
+               }
+               
+            }
+        }
+
+        //MATRIZ MODIFICADA
+        System.out.println("Matriz modificada:");
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.println(matriz[i][j] ? "Verdadero" : "Falso");
+            }
+            System.out.println(); // Salto de línea para separar filas
+        }
+        System.out.println();
+    }   
+}
